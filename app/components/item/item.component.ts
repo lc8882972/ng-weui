0ca80b67c23a117f0734514a475385859d64d9c2
张国延 <410115424@qq.com>
2016-12-03 17:38:47 +0800

    12/3

---
+++ app/components/item/item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Output, OnInit, HostListener, ViewEncapsulation, ElementRef, EventEmitter } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    encapsulation: ViewEncapsulation.None,
+    selector: 'tab-item',
+    templateUrl: 'item.component.html',
+    styleUrls: ['item.component.css']
+})
+export class ItemComponent implements OnInit {
+    @Output('clickme') clickme = new EventEmitter();
+    _active = false;
+    constructor(private ref: ElementRef) { }
+
+    @HostListener('click', ["$event"]) onClick(event: TouchEvent) {
+        this._active = true;
+        this.clickme.emit(this);
+    }
+
+    ngOnInit() {
+       
+    }
+}
