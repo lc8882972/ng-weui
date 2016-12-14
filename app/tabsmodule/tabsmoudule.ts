a6b614d47a05bfc89d2460320a6738bed342c44b
lc8882972 <guoyan2972@qq.com>
2016-12-15 00:05:15 +0800

    手淘flex字体设置

---
+++ app/tabsmodule/tabsmoudule.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { TabsComponent }   from './tabs/tabs.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [TabsComponent],
+    providers: [],
+})
+export class NameModule { }
